79b8442b73e46845a65b319f9c36e5acf746ca85
PrithvijRajesh <143441359+PrithvijRajesh@users.noreply.github.com>
2026-05-12 01:09:48 -0500

    Finished summarizer to give summary of games and testing on ideal final flow

--- agent/summarizer.py
+++ agent/summarizer.py
@@ -25,6 +25,7 @@ def summarize_game(game, reasons):
     loser_top_points = ""
     if game.home_team_score > game.away_team_score:
         winner = game.home_team
+        winner_4q = game.home_analytics.q4.points_scored
         winner_score = game.home_team_score
         winner_top = game.home_impact.top_scorer
         winner_top_points = game.home_impact.top_points
@@ -34,6 +35,7 @@ def summarize_game(game, reasons):
         loser_top_points = game.away_impact.top_points
     else:
         winner = game.away_team
+        winner_4q = game.away_analytics.q4.points_scored
         winner_score = game.away_team_score
         winner_top = game.away_impact.top_scorer
         winner_top_points = game.away_impact.top_points
@@ -43,8 +45,38 @@ def summarize_game(game, reasons):
         loser_top_points = game.home_impact.top_points
 
     #For clutch and comeback games when top scorer on winning team and when top scorer on losing team
-    if winner_top_points>= loser_top_points and "Clutch game" in reasons and "Comeback game" in reasons:
-        summary = f"The {winner} rallied back to beat the {loser} {winner_score} - {loser_score} in a clutch game led by {winner_top}'s {winner_top_points} point game"
-    elif winner_top_points< loser_top_points and "Clutch game" in reasons and "Comeback game" in reasons:
-        summary = f"The {loser} cannot hold off the {winner} {loser_score} - {winner_score} in a clutch game despite {winner_top}'s {winner_top_points} point game"
+    if "Clutch game" in reasons and "Comeback game" in reasons:
+        if winner_top_points >= loser_top_points: 
+            summary = f"The {winner} rallied back to beat the {loser} {winner_score} - {loser_score} in a clutch game led by {winner_top}'s {winner_top_points} point game"
+        else:
+            summary = f"The {loser} caould not hold off the {winner} {loser_score} - {winner_score} comeback in a clutch game despite {loser_top}'s {loser_top_points} point game"
+    elif "Comeback game" in reasons:
+        if winner_top_points >= loser_top_points: 
+            summary = f"The {winner} rallied back with {winner_4q} points in the fourth to beat the {loser} {winner_score} - {loser_score} led by {winner_top}'s {winner_top_points} point game"
+        else:
+            summary = f"The {loser} caould not hold off the {winner} {loser_score} - {winner_score} comeback despite {loser_top}'s {loser_top_points} point game"
+    elif "Clutch game" in reasons:
+        if winner_top_points >= loser_top_points:
+            summary = f"The {winner} managed to hold off the {loser} in a tight {winner_score} - {loser_score} battle where {winner_top} dropped {winner_top_points} points"
+        else:
+            summary = f"{loser_top}'s {loser_top_points} point game goes to waste as the {loser} were unable to hang with the {winner} in the close {winner_score} - {loser_score} battle"
+    elif any("High total score: " in r for r in reasons):
+        summary = f"The {winner} vs. {loser} game was shootout where the {winner}, led by {winner_top}'s {winner_top_points} point game, beat the {loser}, led by {loser_top}'s point game, {winner_score} - {loser_score}"
+    elif f"Post Season game":
+        if winner_top_points >= loser_top_points:
+            summary = f"The {winner} beat the {loser} {winner_score} - {loser_score} in a playoff game led by {winner_top}'s {winner_top_points} point game"
+        else:
+            summary = f"The {loser} lose their playoff game to the {winner} {loser_score} - {winner_score}, despite {loser_top}'s {loser_top_points} points game"
+    elif f"Game has post season implication" in reasons:
+        if winner_top_points >= loser_top_points:
+            summary = f"The {winner} get a crucial {winner_score} - {loser_score} win against the {loser}, on the back of {winner_top}'s {winner_top_points} points"
+        else:
+            summary = f"Despite {loser_top}'s {loser_top_points} point effort, the {loser} drop a crucial game against the {winner}, {loser_score} - {winner_score}"
+    elif any("High scoring " in r for r in reasons):
+        if winner_top_points >= loser_top_points:
+            summary = f"{winner_top}'s {winner_top_points} point game leads the {winner} in a {winner_score} - {loser_score} over the {loser}"
+        else:
+            summary = f"{loser} lose to the {winner} {loser_score} - {winner_score}, despite {loser_top} dropping {loser_top_points} points"
+    else:
+        summary = f"The {winner}, led by {winner_top}'s {winner_top_points} points, beat the {loser}, led by {loser_top}'s {loser_top_points} points, {winner_score} - {loser_score}"
     return summary
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from agent.tests.nba_game_test_data import FAKE_NBA_GAMES
 from agent.decision import evaluate_nba_game
 from config import NBAConfig
 from agent.analysis import analyze_nba_game
+from agent.summarizer import summarize_game
 
 configs = NBAConfig(
     favorite_teams=["Warriors", "Pistons"],
@@ -17,14 +18,21 @@ configs = NBAConfig(
 for game in FAKE_NBA_GAMES:
     important, reasons = evaluate_nba_game(game, configs)
 
-    print(f"{game.home_team} vs {game.away_team}: {important} -> {reasons}")
+    #print(f"{game.home_team} vs {game.away_team}: {important} -> {reasons}")
 
     if important:
-        print("\nGame Analysis:")
+        print("\n Game Summary:")
+        print(summarize_game(game, reasons))
+        user_response = input("Do you want the detailed analysis: ")
+        if user_response.lower() == "yes" or user_response.lower() == "y":
+            print("\nGame Analysis:")
 
-        insights = analyze_nba_game(game)
+            insights = analyze_nba_game(game)
 
-        for line in insights:
-            print(f"- {line}")
+            for line in insights:
+                print(f"- {line}")
 
-        print("\n" + "=" * 50 + "\n")
+            print("\n" + "=" * 50 + "\n")
+            response2 = input("Are you ready for the next game: ")
+            if response2.lower() == "yes" or response2.lower() == "y":
+                continue

00822dda514474308d4bbdbe8482444e4bf7a69d
PrithvijRajesh <143441359+PrithvijRajesh@users.noreply.github.com>
2026-05-13 03:50:17 -0500

    Started working on connecting to ESPN API and understanding data format

--- fetchers/nba_data.py
+++ fetchers/nba_data.py
@@ -1,2 +1,37 @@
 #Sports data fetcher to return raw sports results and data#Sports data fetcher to return raw sports results and data
 
+import requests
+import json
+
+baseURL = "https://site.api.espn.com/apis/site/v2/sports/basketball/nba"
+def fetch_nba_data():
+    url = f"{baseURL}/scoreboard"
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()
+
+data = fetch_nba_data()
+events = data["events"]
+first_game = events[0]
+id = events[0]["id"]
+
+def fetch_summary(event_id: str):
+    url = f"{baseURL}/summary?event={event_id}"
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()
+
+
+summary = fetch_summary(id)
+
+filtered_summary = {
+    "teams": summary["boxscore"]["teams"],
+    "players": summary["boxscore"]["players"],
+    "plays": summary["plays"],
+    "win_probability": summary["winprobability"]
+}
+
+with open("nba_data.json", "w") as f:
+    json.dump(first_game, f, indent = 2)
+with open("nba_summary.json", "w") as f:
+    json.dump(filtered_summary, f, indent = 2)

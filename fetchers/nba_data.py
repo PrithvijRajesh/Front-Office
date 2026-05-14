64709f906c76c12a4061c5d31724f81fe7dd815a
PrithvijRajesh <prithvijr@gmail.com>
2026-05-14 03:19:49 -0700

    Messed with reading the json file and started extracting some stats

--- fetchers/nba_data.py
+++ fetchers/nba_data.py
@@ -31,7 +31,36 @@ filtered_summary = {
     "win_probability": summary["winprobability"]
 }
 
+stats1 = summary["boxscore"]["teams"][0]["statistics"]
+stats2 = summary["boxscore"]["teams"][1]["statistics"]
+team = summary["boxscore"]["teams"]
+
 with open("nba_data.json", "w") as f:
     json.dump(first_game, f, indent = 2)
 with open("nba_summary.json", "w") as f:
     json.dump(filtered_summary, f, indent = 2)
+
+away, home = events[0]["name"].split(" at ")
+print(away)
+print(home)
+
+if team[0]["team"]["displayName"] == away:
+    awayFGM, awayFGA = stats1[0]["displayValue"].split("-")
+    awayFGP = int(awayFGM) / int(awayFGA) * 100
+    awayTPM, awayTPA = stats1[2]["displayValue"].split("-")
+    awayTPP = int(awayTPM) / int(awayTPA) * 100
+    homeFGM, homeFGA = stats2[0]["displayValue"].split("-")
+    homeFGP = int(homeFGM) / int(homeFGA) * 100
+    homeTPM, homeTPA = stats2[2]["displayValue"].split("-")
+    homeTPP = int(homeTPM) / int(homeTPA) * 100
+else:
+    awayFGM, awayFGA = stats2[0]["displayValue"].split("-")
+    awayFGP = int(awayFGM) / int(awayFGA) * 100
+    awayTPM, awayTPA = stats2[2]["displayValue"].split("-")
+    awayTPP = int(awayTPM) / int(awayTPA) * 100
+    homeFGM, homeFGA = stats1[0]["displayValue"].split("-")
+    homeFGP = int(homeFGM) / int(homeFGA) * 100
+    homeTPM, homeTPA = stats1[2]["displayValue"].split("-")
+    homeTPP = int(homeTPM) / int(homeTPA) * 100
+print(away, awayFGM, awayFGA, awayFGP, awayTPM, awayTPA, awayTPP)
+print(home, homeFGM, homeFGA, homeFGP, homeTPM, homeTPA, homeTPP)
